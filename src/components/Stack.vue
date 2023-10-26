6066a8da5ad03665e1f695002bd25023bf670d13
Eoghain O'Briain <eoghain.obriain@appliedtechnology.se>
2023-10-26 15:03:15 +0200

    visual tweaks

--- src/components/Stack.vue
+++ src/components/Stack.vue
@@ -76,7 +76,7 @@ export default {
 .stackBody {
   padding-left: 15px;
   padding-right: 15px;
-  background: rgba(255, 255, 255, 0.1);
+  background-color: rgba(255, 255, 255, 0.1);
 }
 
 .stackLogos {
