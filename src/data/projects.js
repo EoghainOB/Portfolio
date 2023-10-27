325245762c0abbfbeb9e95ab190006292d52b08e
Eoghain O'Briain <eoghain.obriain@appliedtechnology.se>
2023-10-27 12:10:55 +0200

    layout tweaks

--- src/data/projects.js
+++ src/data/projects.js
@@ -5,17 +5,17 @@ export const projects = [
     description:
       "For this project I created a simple application that allowed my young daughter to be able to practice maths. The user can select the type of sum they want to try, addition, subtraction, division or multiplication, as well as the level of difficulty from easy, medium and hard. When they click the start button they are presented with a sum. The user enters the answer into the field and clicks the 'Submit Answer' button. If it is correct the outline will light up green and display the next sum, if it is incorrect it will light up red and the sum will remain.",
     workingUrl: "",
-    stack: ["javascript", "typescript", "nodejs", "react", "html5", "css3"],
+    stack: ["react", "javascript", "typescript", "nodejs", "html5", "css3"],
     repoURL: "https://github.com/EoghainOB/Maths_Test",
   },
   {
-    title: "Rijksmaster - Rijksmuseum Image search",
+    title: "Rijksmuseum Image search",
     image: "/images/projects/rijksmaster.jpeg",
     description:
-      "For this project I was asked to make use of the Rijksmuseum API and create an application that incorporates search. The user can search well known painters and their work in the input field with suggestions taken from a list of popular artists and their artwork. This queries the API and returns the results on a results page, showing the work along with the title of the piece and a link to the page of that piece of art on the Rijksmuseum website.",
+      "For this project I was asked to make use of the Rijksmuseum API and create an application that incorporates search. The user can search well known painters and their work in the input field with suggestions taken from a list of popular artists and their artwork. Users can also search for artwork by century. This queries the API and returns the results on a results page, showing the work along with the title of the piece and a link to the page of that piece of art on the Rijksmuseum website.",
     workingUrl: "",
-    stack: ["javascript", "typescript", "nodejs", "react", "html5", "css3"],
-    repoURL: "https://github.com/EoghainOB/RijksMuseum",
+    stack: ["angular", "javascript", "typescript", "nodejs", "html5", "css3"],
+    repoURL: "https://github.com/EoghainOB/RijksMaster_Angular",
   },
   {
     title: "Milk E-Way Milk Store",
@@ -24,10 +24,10 @@ export const projects = [
       "This was a fun project to work on. I created a 'Milk Store', which contains 99 different types of Milk for sale. The site is made with Typescript on the front and backend and uses Express to interact with the MongoDB, which contains the products. The site integrates pagination as well as filtering and search and each product has its own page with dedicated URL. The user can select the quantity of milk using the slider and it can be added to the cart. The cart is very basic and is my first time creating one and it features the ability to update the cart by deleting the items.",
     workingUrl: "",
     stack: [
+      "react",
       "javascript",
       "typescript",
       "nodejs",
-      "react",
       "express",
       "mongodb",
       "html5",
@@ -41,16 +41,16 @@ export const projects = [
     description:
       "This application has been made using React with Typescript. I uses the https://dummyjson.com/posts API for content and the Unsplash API for photos. The content is initially sorted into 5 groups, which are filtered by tags. The user can further refine what content is displayed using the dropdown at the top of the page which chooses only one group.",
     workingUrl: "",
-    stack: ["javascript", "typescript", "nodejs", "react", "html5", "css3"],
+    stack: ["react", "javascript", "typescript", "nodejs", "html5", "css3"],
     repoURL: "https://github.com/EoghainOB/Tiny-Blog",
   },
   {
-    title: "A simple ToDo list application",
+    title: "ToDo list application",
     image: "/images/projects/todolist.jpeg",
     description:
-      "Made using React with the todos stored to localstorage. The user can add new items to the list, mark them as done, while undo will put it back in the previous location in the list. Of course when the task has been marked as done, it can then be deleted.",
+      "Made using React, the todos are stored in localstorage. The user can add new items to the list, mark them as done, while undo will put it back in the previous location in the list. Of course when the task has been marked as done, it can then be deleted.",
     workingUrl: "",
-    stack: ["javascript", "nodejs", "react", "html5", "css3"],
+    stack: ["react", "javascript", "nodejs", "html5", "css3"],
     repoURL: "https://github.com/EoghainOB/ToDo-List",
   },
   {
@@ -59,16 +59,16 @@ export const projects = [
     description:
       "Made in React, this application performs all the functions of a basic calculator. The user clicks the buttons to set the inputs. I wanted it to look as much like an actual calculator app as possible.",
     workingUrl: "",
-    stack: ["javascript", "nodejs", "react", "html5", "css3"],
+    stack: ["react", "javascript", "nodejs", "html5", "css3"],
     repoURL: "https://github.com/EoghainOB/Calculator",
   },
   {
-    title: "Flight-Finder front end",
+    title: "Flight-Finder",
     image: "/images/projects/flightfinder.jpeg",
     description:
-      "This mobile first front end application was created to work together with the Flight-Finder API (https://github.com/EoghainOB/Flight-Finder). Its a simple app that allows the user to search for flights and to return results with flight and pricing details regardless if it is a direct flight or if it has a layover. It has been created with Typescript and React and I plan to add the ability to select the return flight and purchase in future iterations. ",
+      "This front end application was created to work together with a Flight-Finder API I also created. Its a simple app that allows the user to search for flights and to return results with flight and pricing details regardless if it is a direct flight or if it has a layover. It has been created with Typescript and React.",
     workingUrl: "",
-    stack: ["javascript", "nodejs", "react", "html5", "css3"],
+    stack: ["react", "javascript", "nodejs", "express", "html5", "css3"],
     repoURL: "https://github.com/EoghainOB/Flight-Finder-frontend",
   },
 ];
